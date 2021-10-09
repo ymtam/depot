d2923cbfb25c789068890313de29c8436161d324
ymtam <ymtam81@yahoo.com>
2021-10-09 14:12:21 +0800

    start

--- trains/urls.py
+++ trains/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from trains.views import trainListView, trainUpdateView
 
 urlpatterns = [
-    path('', trainListView.as_view(), name="trains-trains"),
+    path('', trainListView.as_view(), name="trains-list"),
     path('<str:pk>/', trainUpdateView.as_view(), name="trains-update"),
 ]
